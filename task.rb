6c9b8abb49d94f63889563886610cf6930dc0659
takayamori <morit617@gmail.com>
2020-04-04 14:30:06 +0900

    Q6-8の修正、Q10-Q15の回答

--- task.rb
+++ task.rb
@@ -4,8 +4,9 @@
 def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
-  # 以下に回答を記載
-
+  # 以下に回答を記載¥
+  names.<<("斎藤")
+  p names
 end
 
 def q2
@@ -13,21 +14,22 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  array = array1 + array2
+  p array
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  p numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -37,21 +39,23 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts array1.empty?
+  puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map!{|n| n * 10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -60,7 +64,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -70,7 +75,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, n|
+  puts "会員No#{n} #{name}さん"
+  end
 end
 
 def q10
@@ -78,20 +85,28 @@ def q10
 
   # 以下に回答を記載
 
+  if foods.include?("うに")
+    puts "好物です。"
+  else
+    puts "まぁまぁ好きです。"
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten!.uniq!
+  sports.each.with_index(1) do |sport, n|
+    puts "No#{n} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
@@ -99,20 +114,32 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  puts user_data.merge!(update_data)
+  
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
-
+  
   # 以下に回答を記載
-
+  n = data.values
+  puts n
 end
 
 def q15
   data1 = { name: "saitou", hobby: "soccer", age: 33, role: "admin" }
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
+  if data1.include?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
+  if data2.include?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
   # 以下に回答を記載
 
 end
